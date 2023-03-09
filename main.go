9619ee20448adaef1d05f5dfadc7d6f766cb3673
eleven26 <916809498@qq.com>
2023-03-09 21:45:17 +0800

    Add serveHTTP.

--- main.go
+++ main.go
@@ -23,15 +23,22 @@ func main() {
 		Use:   "",
 		Short: "zipkin sampler",
 		Long:  "zipkin sampler",
-		Run:   serve,
-		Args:  cobra.ExactArgs(0),
+		Run: func(cmd *cobra.Command, args []string) {
+			port, _ := cmd.Flags().GetString("port")
+			endpoint, _ := cmd.Flags().GetString("endpoint")
+
+			fmt.Println("listen on port:", port)
+			fmt.Println("zipkin endpoint:", endpoint)
+
+			serveHTTP(port, endpoint)
+		},
+		Args: cobra.ExactArgs(0),
 	}
 
 	cmd.Flags().StringP("port", "p", "9422", "zipkin sampler port.")
 	cmd.Flags().StringP("endpoint", "e", "http://192.168.2.168:9411/api/v2/spans", "zipkin collector endpoint.")
 
-	_ = cmd.MarkFlagRequired("port")
-	_ = cmd.MarkFlagRequired("endpoint")
+	//_ = cmd.MarkFlagRequired("endpoint")
 
 	err := cmd.Execute()
 	if err != nil {
@@ -39,20 +46,17 @@ func main() {
 	}
 }
 
-func serve(cmd *cobra.Command, args []string) {
-	port, _ := cmd.Flags().GetString("port")
-	endpoint, _ := cmd.Flags().GetString("endpoint")
+func serveHTTP(port, endpoint string) {
+	reporter := sampler.NewReporter(
+		endpoint, // zipkin server
+		sampler.NewTimeBaseSampler(time.Second*5), // 采集超过5秒的trace
+	)
+	collector := sampler.NewCollector(
+		time.Minute*10, // 超过 30分钟，没有等到 root trace 就丢弃
+		reporter,
+	)
 
-	fmt.Println("listen on port:", port)
-	fmt.Println("zipkin endpoint:", endpoint)
-	http.HandleFunc("/traces", handler(endpoint))
-
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
-
-// todo fix post to zipkin
-func handler(endpoint string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/traces", func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
 				log.Error("panic error.")
@@ -63,13 +67,6 @@ func handler(endpoint string) http.HandlerFunc {
 			}
 		}()
 
-		collector := sampler.NewCollector(
-			time.Minute*10, // 超过 30分钟，没有等到 root trace 就丢弃
-			sampler.NewReporter(
-				endpoint, // zipkin server
-				sampler.NewTimeBaseSampler(time.Second*5), // 采集超过5秒的trace
-			),
-		)
 		err := collector.Collect(r.Body)
 		if err != nil {
 			log.Error("collect error.")
@@ -81,7 +78,8 @@ func handler(endpoint string) http.HandlerFunc {
 			return
 		}
 
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write(([]byte)("ok."))
-	}
+		w.WriteHeader(http.StatusAccepted)
+	})
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
